01625fa3157b5a9855ff5055aaf1fc6e3d8f3cd6
Halithan <halithan96@hotmail.com>
2019-05-07 11:59:31 +0200

    Added explanation to step in MAIN (dutch)

--- HC_Test/hc_MAIN.py
+++ HC_Test/hc_MAIN.py
@@ -18,6 +18,14 @@ import copy
 from Sphero_test import Sphero
 from test_slam import Particle, FastSLAM
 
+
+"""Start knop: 
+0.Geeft alle waarden die binnen komen als global
+1.Zorgt ervoor dat we beginnen met t=0 (tijd)
+2.Start het proces door loop = not(loop)
+3.Voert het proces uit door "while loop"
+4.Wanneer pose niet gelijk is aan -1 dan zal het een measurement uitvoeren voor de robotmap (recht kaart)
+"""
 def start():
     global pose, control, constraints, loop, radius, wall_width, world_size, flag, lw
     scale = 520
@@ -42,10 +50,14 @@ def start():
         pose = pose[0]
 #         i = i + 1
 
+"Stop knop: Dit stopt de Loop met "loop = not(loop")
+
 def stop():
     global loop, lw
     loop = not(loop)
     print "Tree",lw
+
+"Boost/Slow knop: Veranderd snelheid door control +- 50 te nemen, waarbij control=0 als minimum"
 
 def boost():
     global control
@@ -59,6 +71,7 @@ def slow():
     if control[0] <= 0:
         control[0] =0
 
+"Correct wall knop: hoe dit precies werkt weet ik nog niet"
 def correct_wall():
     global pose, control, loop, flag
     loop = not(loop)
@@ -67,6 +80,7 @@ def correct_wall():
     loop = 0
     flag = 1
         
+"De Functie wat terugkomt in Startknop en ervoor zorgt dat de wall getekend in figure 2."
 def robot_map(wall,t):
     p = []
     for i in range(len(wall)): 
@@ -165,6 +179,8 @@ def robot_map(wall,t):
             cb1.outline.set_visible(False)
             plt.gcf().canvas.draw()
 
+
+"Hier worden de bestanden binnen hc_wall uitgevoerd en geeft een canvas links"
 # Initialization of GUI
 bounds = boundaries(world_size,wall_width) 
 walls = wall(wall_width,wall_location)
@@ -174,7 +190,7 @@ frame1.pack(fill=BOTH)
 wall_canvas = Canvas(frame1,width=500,height=500,bg="white")
 wall_canvas.pack(side=LEFT)
 
-
+"Dit zorgt ervoor dat een figure recht word geplot welke later worden ingevoerd door: def robot_map"
 # Robot map
 r = Canvas(frame1,width=500,height=500,bg="white")
 r.pack(side=RIGHT)
@@ -182,27 +198,34 @@ fig = plt.figure(figsize=(6.15,6.15),facecolor='w',edgecolor='w')
 canvas = FigureCanvasTkAgg(fig, master=r)
 canvas.get_tk_widget().pack(side=RIGHT)
 
+"Dit vult de canvas aan de linker kant met getekende muren"
 # Real MAp
-walls.draw(wall_width, wall_location, wall_canvas)
-bounds.draw(origin, wall_canvas)
+walls.draw(wall_width, wall_location, wall_canvas) #tekent binnenmuren
+bounds.draw(origin, wall_canvas) #rand muren
 
-parray = bounds.boundary_grid(origin,wall_canvas)
-warray = walls.wall_grid(wall_width,wall_location,wall_canvas)
+warray = walls.wall_grid(wall_width,wall_location,wall_canvas) # grid voor binnenmuren
+parray = bounds.boundary_grid(origin,wall_canvas) # grid voor rand muren
 
+
+"""
+1. Hier worden de bestanden binnen Sphero_test.py ingevoerd.
+2. Hier worden de werkelijke muren waar de sphero rekening mee moet houden ingevoerd (constraints)
+3. Hier wordt het Slam algoritme binnen test_slam.py ingevoerd met functie: Particle en FastSLAM
+
+"""
 # Creating Sphero robot
-s = Sphero(wall_canvas)
-constraints = s.sphero_constraint(origin,wall_width,world_size,wall_location)
+s = Sphero(wall_canvas) #Haalt informatie uit Sphero file
+constraints = s.sphero_constraint(origin,wall_width,world_size,wall_location) # informatie over muren
 
-initial_particles = [copy.copy(Particle(pose))
-                        for _ in xrange(number_of_particles)]
+initial_particles = [copy.copy(Particle(pose)) 
+                        for _ in xrange(number_of_particles)] # SLAM algoritme
 
 fs = FastSLAM(initial_particles,robot_width,minimum_correspondence_likelihood,measurement_stddev,xstddev,ystddev,
-                control_speed_factor,control_head_factor, sample_time)
+                control_speed_factor,control_head_factor, sample_time) #FastSLAM algoritme
 
 
-
+"Hier worden de knoppen welke boven aan de file als definition waren beschreven als functie binnen de Tkinter canvas verwerkt"
 #KNOPPEN
-
 frame2 = Frame(root)
 frame2.pack()
 start = Button(frame2,text="START", width=10,command=start)
