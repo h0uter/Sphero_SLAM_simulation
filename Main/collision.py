46b6c6e297f82257e2b31a2e33d7662f2e9584bc
Wouter <houterm@gmail.com>
2019-05-08 16:05:43 +0200

    sphero.center_x,y property added

--- Main/collision.py
+++ Main/collision.py
@@ -4,15 +4,15 @@ import world as w
 # here we can plugin the SLAM algorithm
 def collision_check(sphero):
     if sphero.x_right >= w.WIDTH:
-        print ("collision right")
+        print ("collision at [{0}, {1}]".format(sphero.x_right, sphero.center_y))
         sphero.speed_x = -sphero.speed_x
     if sphero.x_left <= 0:
-        print ("collision left")
+        print ("collision at [{0}, {1}]".format(sphero.x_left, sphero.center_y))
         sphero.speed_x = -sphero.speed_x
     if sphero.y_bottom >= w.HEIGHT:
         #TODO: log coordinates of the wall
-        print ("collision bottom at {0}".format(sphero.x_left + 25))
+        print ("collision at [{0}, {1}]".format(sphero.center_x, sphero.y_bottom))
         sphero.speed_y = -sphero.speed_y
     if sphero.y_top <= 0:
-        print ("collision top")
+        print ("collision at [{0}, {1}]".format(sphero.center_x, sphero.y_top))
         sphero.speed_y = -sphero.speed_y
--- Main/sphero.py
+++ Main/sphero.py
@@ -3,18 +3,24 @@ import numpy as np
 
 class Sphero:
   def __init__(self):
+
+
+
     self.x_left = 225
     self.y_top = 225
     self.x_right = 275
     self.y_bottom = 275
+    
+    self.center_x = self.x_right - 25
+    self.center_y = self.y_bottom - 25
 
     self.speed_x = 13
     self.speed_y = 9
     # Create a vector as a column
-    velocity = np.array([[self.speed_x],
-                        [self.speed_y]])
+    # velocity = np.array([[self.speed_x],
+    #                     [self.speed_y]])
 
-    d = {'x': self.speed_x, 'y': self.speed_x}
+    # d = {'x': self.speed_x, 'y': self.speed_x}
 
   def draw(self,canvas):
     return canvas.create_oval(self.x_left, self.y_top, self.x_right, self.y_bottom, fill="blue", tag='ball')
