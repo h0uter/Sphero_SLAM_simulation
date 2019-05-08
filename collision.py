24b36db2a30302539e30d28dbb620eb41b2e88ed
Wouter <houterm@gmail.com>
2019-05-08 09:51:49 +0200

    collision logging

--- collision.py
+++ collision.py
@@ -1,12 +1,17 @@
 # describes the collision process
 import world as w
 
+# here we can plugin the SLAM algorithm
 def collision_check(sphero):
     if sphero.x_right >= w.WIDTH:
+        print ("collision right")
         sphero.speed_x = -sphero.speed_x
     if sphero.x_left <= 0:
+        print ("collision left")
         sphero.speed_x = -sphero.speed_x
     if sphero.y_bottom >= w.HEIGHT:
+        print ("collision botom")
         sphero.speed_y = -sphero.speed_y
     if sphero.y_top <= 0:
+        print ("collision top")
         sphero.speed_y = -sphero.speed_y
