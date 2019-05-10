75f469ec6ace44be057179a91994b8bab11e65c5
Wouter <houterm@gmail.com>
2019-05-10 16:11:52 +0200

    draw circle from x,y,r

--- Main/sphero.py
+++ Main/sphero.py
@@ -1,29 +1,24 @@
 # Describes a Sphero agent which can move through the world and collide with walls
 import numpy as np
+from view import _create_circle
 
 class Sphero:
-  def __init__(self):
+  def __init__(self, x=250, y=250, r=50):
     
-    self.x_left = 225
-    self.y_top = 225
-    self.x_right = 275
-    self.y_bottom = 275
+    self.x_left = x-r
+    self.y_top = y-r
+    self.x_right = x+r
+    self.y_bottom = y+r
     
-    self.center_x = self.x_right - 25
-    self.center_y = self.y_bottom - 25
+    self.center_x = x
+    self.center_y = y
 
     self.speed_x = 13
     self.speed_y = 9
 
-    # TODO: position & velocity propperty
-    # Create a vector as a column
-    # velocity = np.array([[self.speed_x],
-    #                     [self.speed_y]])
-
-    # d = {'x': self.speed_x, 'y': self.speed_x}
-
-  def draw(self,canvas):
+  def draw(self, canvas):
     return canvas.create_oval(self.x_left, self.y_top, self.x_right, self.y_bottom, fill="blue", tag='ball')
+    # return canvas.create_oval(x-r, y-r, x+r, y+r, fill="blue", tag='ball')
 
   def move(self):
     self.x_left += self.speed_x
--- Main/view.py
+++ Main/view.py
@@ -1,23 +1,85 @@
 # outputs a topdown view of the world
 # overlayed with the position of the Sphero and the map it has created of it's environment
-from tkinter import Tk, Canvas, mainloop
-from sphero import Sphero
-from collision import collision_check
-import world as w
+import tkinter as tk
 
-# TODO: view is not yet implemented 
+def _create_circle(self, x, y, r, **kwargs):
+    """Create a circle
+        
+    x the abscissa of centre
+    y the ordinate of centre
+    r the radius of circle
+    **kwargs optional arguments
+    return the drawing of a circle
+    """
+    return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
+tk.Canvas.create_circle = _create_circle
 
-def create_view():
-  window = Tk()
-  canvas = Canvas(window, width=w.WIDTH, height=w.HEIGHT, background="green")
-  canvas.pack()
-  return window, canvas
+def _coords_circle(self, target, x, y, r, **kwargs):
+    """Define a circle
+        
+    target the circle object
+    x the abscissa of centre
+    y the ordinate of centre
+    r the radius of circle
+    **kwargs optional arguments
+    return the circle drawing with updated coordinates
+    """
+    return self.coords(target, x-r, y-r, x+r, y+r, **kwargs)
+tk.Canvas.coords_circle = _coords_circle
 
-def update_view(canvas):
-  canvas.move("ball", sphero.speed_x, sphero.speed_y)
-  canvas.after(30)
-  canvas.update()  
+class View:
+  def __init__(self, step, size):
+    """Initialize and launch the view"""
+    self.step = step
+    self.size = size
 
-# window = Tk()
-# canvas = Canvas(window, width=w.WIDTH, height=w.HEIGHT, background="green")
-# canvas.pack()
+    self.window = tk.Tk()
+    self.canvas = tk.Canvas(self.window, width=self.size, height=self.size, bg="black")
+    self.canvas.pack()
+    self.canvas.focus_set()
+    self.drawing = self.create()
+    self.started = False
+
+    start_button = tk.Button(self.window, text="Start", command=self.start)
+    stop_button = tk.Button(self.window, text="Pause", command=self.stop)
+    start_button.pack()
+    stop_button.pack()
+
+    self.window.mainloop()
+    
+    def create(self):
+        """Create a drawing item for each solver.Ball object
+            
+        return a dictionary with solver.Ball objects as keys and their circle drawings as items
+        """
+        return {ball: self.canvas.create_circle(ball.position[0], ball.position[1], ball.radius, fill="white") for ball in self.balls}
+
+    def update(self):
+        """Update the drawing items for a time step"""
+        solver.solve_step(self.balls, self.step, self.size)
+        for ball in self.balls:
+            self.canvas.coords_circle(self.drawing[ball], ball.position[0], ball.position[1], ball.radius)
+        self.canvas.update()
+
+    def start(self):
+        """Start the animation"""
+        if not self.started:
+            self.started = True
+            self.animate()
+
+    def animate(self):
+        """Animate the drawing items"""
+        if self.started:
+            self.update()
+            self.window.after(0, self.animate)
+
+    def stop(self):
+        """Stop the animation"""
+        self.started = False
+
+# Test this module
+if __name__ == "__main__":
+    balls = [solver.Ball(20., 20., [40.,40.], [5.,5.]), solver.Ball(10., 10., [480.,480.], [-15.,-15.]), solver.Ball(15., 15., [30.,470.], [10.,-10.])]
+    size = 500.
+    step = 0.01
+    Display(balls, step, size)
