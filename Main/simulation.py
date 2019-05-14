72c70291abdc224cacd36eeaf00630744703aa54
Halithan <halithan96@hotmail.com>
2019-05-14 09:50:11 +0200

    Debugged error, cleaned merge

--- Main/simulation.py
+++ Main/simulation.py
@@ -16,21 +16,24 @@ def init_list(N):
         balls.append(Sphero(m, m, vx, vv))
     return balls
 
-"""parameters to run the simulation with"""        
+"""parameters to run the simulation with"""       
+
 if __name__ == "__main__":
     m = 20.
     r = 10.
     size = 500.
-    step = 0.01
-    startposition_ball2 =[40.,40.]
+    step = 0.07
+    startposition_ball1 =[140.,320.]
+    startposition_ball2 =[200.,130.]
     startposition_ball3 =[40.,120.]
 
-    spheros = [ solver.Sphero(m, r, startposition_ball1, [-8.,-8.]), 
-                solver.Sphero(m, r, startposition_ball2, [6.,5.]), 
+    
+
+    spheros = [ solver.Sphero(m, r, startposition_ball1, [-10.,-10.]), 
+                solver.Sphero(m, r, startposition_ball2, [10.,10.]), 
                 solver.Sphero(m, r, startposition_ball3, [10.,15.])]
 
     walls = [Wall([100,0,110,300]), Wall([400,0,410,300]), Wall([250, int(size-300), 260, int(size)])]
-    startposition_ball1 =[300.,400.]
-    spheros = [solver.Sphero(20., 15., [140.,320.], [-10.,-10.]), solver.Sphero(20., 15., [200.,130.], [10.,10.]), solver.Sphero(20., 15., [40.,120.], [10.,15.])]
+    
 
     Display(spheros, walls, step, size)
